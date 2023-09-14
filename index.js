b0c58bc0c7c9bb625a5d1f943f493c5e2c0d7207
frank-mck <fmckenna56330@gmail.com>
2023-09-14 16:46:06 +0100

    chore: add ids and create songs object

---
+++ index.js
@@ -0,0 +1,31 @@
+export const songs = [
+  {
+    title: 'Ambient',
+    artist: 'No name',
+    url: 'https://raw.githubusercontent.com/florinpop17/stream-songs/master/ambient.mp3',
+    thumbnail: 'https://images.unsplash.com/photo-1534790021298-16d65d290461?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
+  },
+  {
+    title: 'Garage',
+    artist: 'No name',
+    url: 'https://raw.githubusercontent.com/florinpop17/stream-songs/master/garage.mp3',
+    thumbnail: 'https://images.unsplash.com/photo-1551522435-a13afa10f103?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2340&q=80',
+  },{
+    title: 'Night Vlog',
+    artist: 'No name',
+    url: 'https://raw.githubusercontent.com/florinpop17/stream-songs/master/night-vlog.mp3',
+    thumbnail: 'https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2187&q=80',
+  },
+  {
+    title: 'Study',
+    artist: 'No name',
+    url: 'https://raw.githubusercontent.com/florinpop17/stream-songs/master/study.mp3',
+    thumbnail: 'https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2273&q=80',
+  },
+  {
+    title: 'Forest',
+    artist: 'No name',
+    url: 'https://raw.githubusercontent.com/florinpop17/stream-songs/master/forest.mp3',
+    thumbnail: 'https://images.unsplash.com/photo-1511497584788-876760111969?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2232&q=80',
+  }
+]
