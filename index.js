1d7fb8e00755596576b1e04b6291594913a69a94
frank-mck <fmckenna56330@gmail.com>
2023-09-14 20:22:17 +0100

    feat: render songs to the dom

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-export const songs = [
+const songs = [
   {
     title: 'Ambient',
     artist: 'No name',
@@ -29,3 +29,38 @@ export const songs = [
     thumbnail: 'https://images.unsplash.com/photo-1511497584788-876760111969?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2232&q=80',
   }
 ]
+
+const titleEl = document.querySelector('#title');
+const artistEl = document.querySelector('#artist');
+const thumbnailEl = document.querySelector('#thumbnail');
+const progressEl = document.querySelector('#progress');
+const currentTimeEl = document.querySelector('#current-time');
+const totalTimeEl = document.querySelector('#total-time');
+const repeatBtn = document.querySelector('#repeat-btn');
+const previousBtn = document.querySelector('#prev-btn');
+const toggleBtn = document.querySelector('#toggle-btn');
+const nextBtn = document.querySelector('#next-btn');
+const shuffleBtn = document.querySelector('#shuffle-btn');
+const playlistEl = document.querySelector('#playlist');
+
+songs.forEach((song) => {
+  const songBtn = document.createElement('button');
+  const songEl = document.createElement('audio');
+
+  songBtn.className = 'py-2 flex justify-between w-full hover:font-bold';
+ 
+  songEl.src = song.url;
+  songEl.controls = true;
+
+  playlistEl.appendChild(songEl);
+
+  songEl.addEventListener('loadedmetadata', () => {
+    songBtn.innerHTML = `${song.title} <span>${getReadableTime(songEl.duration)}</span>`;
+    playlistEl.appendChild(songBtn);
+  })
+
+  function getReadableTime(duration) {
+    return `${Math.floor(duration / 60)}:${`${Math.floor(duration) % 60}`.padStart(2, '0')}`;
+  }
+  
+})
