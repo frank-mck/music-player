9e2df4517e93370c3ed083e4605511634c18b920
frank-mck <fmckenna56330@gmail.com>
2023-09-15 14:15:50 +0100

    feat: add dynamic content to buttons and progress

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const songs = [
   {
     title: 'Ambient',
-    artist: 'No name',
+    artist: 'Foot steps on the moon',
     url: 'https://raw.githubusercontent.com/florinpop17/stream-songs/master/ambient.mp3',
     thumbnail: 'https://images.unsplash.com/photo-1534790021298-16d65d290461?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80',
   },
@@ -30,6 +30,8 @@ const songs = [
   }
 ]
 
+let activeSong = undefined;
+
 const titleEl = document.querySelector('#title');
 const artistEl = document.querySelector('#artist');
 const thumbnailEl = document.querySelector('#thumbnail');
@@ -42,8 +44,10 @@ const toggleBtn = document.querySelector('#toggle-btn');
 const nextBtn = document.querySelector('#next-btn');
 const shuffleBtn = document.querySelector('#shuffle-btn');
 const playlistEl = document.querySelector('#playlist');
+const playbtn = document.querySelector("[data-target-play]");
+const pausebtn = document.querySelector("[data-target-pause]");
 
-songs.forEach((song) => {
+songs.forEach((song, index) => {
   const songBtn = document.createElement('button');
   const songEl = document.createElement('audio');
 
@@ -55,9 +59,47 @@ songs.forEach((song) => {
   playlistEl.appendChild(songEl);
 
   songEl.addEventListener('loadedmetadata', () => {
-    songBtn.innerHTML = `${song.title} <span>${getReadableTime(songEl.duration)}</span>`;
+    const time = getReadableTime(songEl.duration);
+    song.time = time;
+
+    songBtn.innerHTML = `${song.title} <span>${time}</span>`;
     playlistEl.appendChild(songBtn);
+
+    if (index === 0) {
+      currentTimeEl.innerText =`0:00`;
+      totalTimeEl.innerText = time;
+      getSongDetails(song);
+    }
   })
+
+  songEl.addEventListener("timeupdate", () => {
+    const { currentTime } = songEl;
+    updateProgress(currentTime)
+    currentTimeEl.innerText = getReadableTime(currentTime)
+  });
+
+  toggleBtn.addEventListener('click', () => {
+    playbtn.classList.toggle('hidden');
+    pausebtn.classList.toggle('hidden');
+
+    if (activeSong.paused) {
+      activeSong.play();
+    } else {
+      activeSong.pause();
+    }
+  })
+
+  function updateProgress(time) {
+    progressEl.value = time / (activeSong.duration * 100);
+  }
+
+  function getSongDetails(song) {
+    activeSong = songEl;
+    totalTimeEl.innerText = song.time;
+    thumbnailEl.src = song.thumbnail;
+    titleEl.innerText = song.title;
+    artistEl.innerText = song.artist;
+  }
 
   function getReadableTime(duration) {
     return `${Math.floor(duration / 60)}:${`${Math.floor(duration) % 60}`.padStart(2, '0')}`;
